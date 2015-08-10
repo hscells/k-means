daf5598832ebd4012fbb629f7e2ebed3b93c6993
Harry Scells <harryscells@gmail.com>
2015-08-10 11:54:01 +1000

    added some tests

--- test/k_means/core_test.clj
+++ test/k_means/core_test.clj
@@ -1,3 +1,11 @@
 (ns k-means.core-test
   (:require [clojure.test :refer :all]
             [k-means.core :refer :all]))
+
+(deftest mean-test
+  (testing "means"
+    (is (= 2.5 (mean [1 2 3 4])))))
+
+(deftest distance-test
+  (testing "distance"
+    (is (= 5.0 (distance [2 -1] [-2 2])))))
