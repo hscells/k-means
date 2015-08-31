684277c7e10bc41dd98f8f183f00ad7cef48386c
Harry Scells <harryscells@gmail.com>
2015-08-31 19:13:52 +1000

    profile the code

--- project.clj
+++ project.clj
@@ -3,9 +3,11 @@
   :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"] [com.taoensso/timbre "4.1.1"]]
   :main ^:skip-aot k-means.core
   :target-path "target/%s"
   :plugins [[codox "0.8.13"]]
   :codox {:output-dir "hscells.github.io/k-means/doc"}
-  :profiles {:uberjar {:aot :all}})
+  :profiles { :uberjar {:aot :all}
+              :debug {:timbre {:current-level :trace}}
+              :dev {:timbre {:current-level :debug}}})
