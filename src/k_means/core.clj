15ac455271cfb7065ad477ebc439a3c80dbc7725
Harry Scells <harryscells@gmail.com>
2015-09-14 18:47:07 +1000

    use lists rather than vecs

--- src/k_means/core.clj
+++ src/k_means/core.clj
@@ -7,7 +7,7 @@
 
 (defn choose-initial-centroids
   "Randomly choose k centroids from vectors"
-  ([l k] (choose-initial-centroids l k (vector)))
+  ([l k] (choose-initial-centroids l k (list)))
   ([l k c]
     (cond
       (< k 1) c
@@ -34,18 +34,18 @@
 (defn k-means-p
   "Perform k-means using concurrent paradigms"
   ([l k max-iterations]
-    (k-means-p l k (p :choose-initial-centroids (choose-initial-centroids l k)) (vector/make-vector k) max-iterations))
+    (k-means-p l k (p :choose-initial-centroids (choose-initial-centroids l k)) (vector/make-list k) max-iterations))
   ([l k c g max-iterations]
-    (let [t c g (p :cluster-parallel (cluster/cluster-p l c)) c (p :mean-centroids (cluster/mean-centroids g))]
+    (let [t c g (p :cluster-parallel (cluster/cluster-p l c)) c (p :mean-centroids (cluster/mean-centroids-p g))]
       (println "Iteration" (- 100 max-iterations) c)
       (cond
         (> 0 max-iterations) g
         (still-moving? t c) ; Keep shifting centroids until equilibrium
           (recur l k c g (dec max-iterations))
-        :else (vec g)))))
+        :else g))))
 
 (defn -main
   "Main function for k-means"
   [& args]
-  (let [c (profile :info :Arithmetic (k-means-p (vector/make-vector-rand 40000) 5 100)) => "Done"]
-    (spit "k-means.txt" c)) (shutdown-agents))
+  (let [c (profile :info :Arithmetic (k-means (vector/make-list-rand 5000) 7 15)) => "Done"]
+    (spit "k-means.txt" (vec c))) (shutdown-agents))
