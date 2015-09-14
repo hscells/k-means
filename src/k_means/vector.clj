ab70b22d923fa099f3e3c3d6a1202ab382d6ba72
Harry Scells <harryscells@gmail.com>
2015-09-14 18:46:39 +1000

    can make lists and vectors

--- src/k_means/vector.clj
+++ src/k_means/vector.clj
@@ -28,3 +28,22 @@
       (<= n 0) c
       :else
         (recur (dec n) (conj c [])))))
+
+
+(defn make-list-rand
+  "create a vector containing n cols populated with random data"
+  ([n] (make-vector-rand n '()))
+  ([n c]
+  (cond
+    (<= n 0) c
+    :else
+      (recur (dec n) (conj c '((rand-int 50000) (rand-int 50000)))))))
+
+(defn make-list
+  "create a vector containing n cols"
+  ([n] (make-vector n []))
+  ([n c]
+    (cond
+      (<= n 0) c
+      :else
+        (recur (dec n) (conj c '())))))
